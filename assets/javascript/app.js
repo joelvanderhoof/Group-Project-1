806240283c505c9cee4ffbb41480ef7979cfccf0
Anthony Morreale <anthony_morreale@dhcp-v069-181.mobile.uci.edu>
2017-05-04 20:46:02 -0700

    javascript background

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+$(function () {
+    var body = $('body');
+    var backgrounds = [
+      'url(../images/background_img/cityscape.png)', 
+      'url(../images/background_img/forest.jpeg)', 
+      'url(../images/background_img/mountain-village.png)', 
+      'url(../images/background_img/ocean-village.jpeg)'];
+    var current = 0;
+
+    function nextBackground() {
+        body.css(
+            'background',
+        backgrounds[current = ++current % backgrounds.length]);
+
+        setTimeout(nextBackground, 5000);
+    }
+    setTimeout(nextBackground, 5000);
+    body.css('background', backgrounds[0]);
+});
