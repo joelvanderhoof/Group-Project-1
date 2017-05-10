8f87de28c44e95153c234f6f24312ac62e514bda
Joel Vanderhoof <joelvanderhoof@yahoo.com>
2017-05-09 19:54:05 -0700

    Added Express server on NOde js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,37 @@
-$(function () {
-    var body = $('body');
-    var backgrounds = [
-      'url("assets/images/background_img/cityscape.png")', 
-      'url("assets/images/background_img/forest.jpg")', 
-      'url("assets/images/background_img/mountain-village.png")', 
-      'url("assets/images/background_img/ocean-village.jpg")'];
-    var current = 0;
+// $(function () {
+//     var body = $('body');
+//     var backgrounds = [
+//       'url("assets/images/background_img/cityscape.png")', 
+//       'url("assets/images/background_img/forest.jpg")', 
+//       'url("assets/images/background_img/mountain-village.png")', 
+//       'url("assets/images/background_img/ocean-village.jpg")'];
+//     var current = 0;
 
-    function nextBackground() {
-        body.css(
-            'background',
-        backgrounds[current = ++current % backgrounds.length]);
+//     function nextBackground() {
+//         body.css(
+//             'background',
+//         backgrounds[current = ++current % backgrounds.length]);
 
-        setTimeout(nextBackground, 5000);
-    }
-    setTimeout(nextBackground, 5000);
-    body.css('background', backgrounds[0]);
+//         setTimeout(nextBackground, 5000);
+//     }
+//     setTimeout(nextBackground, 5000);
+//     body.css('background', backgrounds[0]);
+// });
+
+
+var reqObject = JSON.stringify({ "term": "coffee", "latitude": "33.645068", "longitude": "-117.835098", "radius": "500"});
+
+//test Yelp API call through server
+var settings = {
+  "url": "http://localhost:3000/yelp",
+  "method": "POST",
+  "headers": {
+    "content-type": "application/json; charset=UTF-8",
+  },
+  "data": reqObject
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
 });
+
---
+++ server.js
@@ -0,0 +1,83 @@
+var path = require("path");
+var bodyParser = require("body-parser");
+//var Yelp = require("yelp");
+var express = require("express");
+var request = require("request");
+
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+//
+
+
+//Serves the root directory relative to the directory that the express app is run from
+app.use(express.static(path.join(__dirname, "/")));
+
+app.listen(port, function () {
+    console.log("The server is listening on port " + port);
+});
+
+// Sends the index.html file as response when the "/" route is called
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/yelp", function (req, res) {	
+	console.log(req.body);
+
+	var queryURL = "https://api.yelp.com/v3/businesses/search?";
+	var options = { 
+		method: "GET",
+		url: queryURL,
+		qs: req.body,
+		headers: { 
+	   		//"postman-token": "d4acc441-7e71-ad84-dddb-e88e563ba45b",
+	     	"cache-control": "no-cache",
+	     	"authorization": "Bearer S7LImio1bL-jYfN1zzz6z6iKmrFE95eCDH9nQodFeB2Ms2vW4UYYaBTX256MW0B52uqk1_N1w4F2FunzAR89rXa6-3L0r1TCr7bfsUKtAQtApnyZUhfwtb3D5OgLWXYx",
+	     	"content-type": "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" 
+	     },
+	  		"formData": { 
+	  			oauth_token: "S7LImio1bL-jYfN1zzz6z6iKmrFE95eCDH9nQodFeB2Ms2vW4UYYaBTX256MW0B52uqk1_N1w4F2FunzAR89rXa6-3L0r1TCr7bfsUKtAQtApnyZUhfwtb3D5OgLWXYx" 
+			} 
+	};
+
+	request(options).pipe(res);
+
+ });
+
+app.post("/green", function (req, res) {	
+	console.log(req.body.qs);
+	var queryURL = "https://api.airbnb.com/v2/search_results?client_id=3092nxybyb0otqw18e8nh5nty" + req.body.qs;
+	var options = { 
+		method: "GET",
+		url: queryURL,
+		//qs: req.body.qs,
+		// headers: { 
+	 //   		//"postman-token": "d4acc441-7e71-ad84-dddb-e88e563ba45b",
+	 //     	"cache-control": "no-cache",
+	 //     	"authorization": "Bearer S7LImio1bL-jYfN1zzz6z6iKmrFE95eCDH9nQodFeB2Ms2vW4UYYaBTX256MW0B52uqk1_N1w4F2FunzAR89rXa6-3L0r1TCr7bfsUKtAQtApnyZUhfwtb3D5OgLWXYx",
+	 //     	"content-type": "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" 
+	 //     },
+	 //  		"formData": { 
+	 //  			oauth_token: "S7LImio1bL-jYfN1zzz6z6iKmrFE95eCDH9nQodFeB2Ms2vW4UYYaBTX256MW0B52uqk1_N1w4F2FunzAR89rXa6-3L0r1TCr7bfsUKtAQtApnyZUhfwtb3D5OgLWXYx" 
+		// 	} 
+	};
+
+	request(options).pipe(res);
+	
+
+ });
+
+
+
+
+
+
+
+
+
+
